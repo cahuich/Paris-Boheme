e4ffcaac56400ec978cf288267781d0a900abbeb
Philippe JOSEPH-ROSE <63668052+cahuich@users.noreply.github.com>
2026-02-13 22:10:50 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Paris-Boheme/', // reemplaza TU_REPO por el nombre de tu repo
   plugins: [react()],
+  base: '/Paris-Boheme/',  
 })
