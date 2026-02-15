db9345fe325d20f661a9352056ed818254f52566
cahuich <pjosephrose@gmail.com>
2026-02-15 01:25:20 -0500

    Update Frontend

---
+++ frontend/src/main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css"; // o App.css según tu configuración
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
